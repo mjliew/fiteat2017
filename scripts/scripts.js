ed1048fb31d61473b66968304fffd5964097d956
mjliew <mjliew@tlu.ee>
2017-04-12 03:33:38 +0300

    FAQ page update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,14 @@ $(window).load(function() {
     animation: "slide"
   });
 });
+
+//this causes errors wen #qa-answers are made to toggle
+$('#qa-click-switch-icon-error').click(function() {
+    $('#qa-answers').toggle('1000');
+    $("i", this).toggleClass("fa fa-minus-circle fa fa-plus-circle");
+});
+
+//but without it I can toggle the icons, but it won't close/open the answers
+$('#qa-switch-icon-only').click(function() {
+    $("i", this).toggleClass("fa fa-plus-circle fa fa-minus-circle");
+});
